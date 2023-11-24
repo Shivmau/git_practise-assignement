55ddd331d813404ec6cab1de809a0da7c4861008
Shivam Maurya <shivamgonda551@gmail.com>
2023-11-23 22:15:19 -0800

    prime-no

---
+++ prime-no.js
@@ -0,0 +1,15 @@
+N=13
+let count=1
+for (i=0; i<=N; i++){
+  if(N%i==0){
+   count=count+1
+   }
+}
+
+if(count==2){
+   console.log("Prime_number")
+   }
+else{
+   console.log("Not_prime_number")
+   }
+    
